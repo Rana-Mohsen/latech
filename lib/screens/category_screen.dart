6385ffaa0f89c1b9a970880cd3104335dbf64fe2
Rana-3r2m <rana3r2m@gmail.com>
2023-08-19 04:36:08 +0300

    bottom nav bar

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home_layout.dart
+++ lib/screens/home_layout.dart
@@ -1,3 +1,8 @@
+import 'package:final_pro/screens/Home_screen.dart';
+import 'package:final_pro/screens/app_info.dart';
+import 'package:final_pro/screens/category_screen.dart';
+import 'package:final_pro/screens/profile.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeLayout extends StatefulWidget {
@@ -8,8 +13,40 @@ class HomeLayout extends StatefulWidget {
 }
 
 class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex = 0;
+  List<Widget> pages = [
+    HomeScreen(),
+    CategoryScreen(),
+    ProfileScreen(),
+    AppinfoScreen()
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Color(0xff0001fc),
+        backgroundColor: Color(0xffeff5fb),
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_sharp), label: 'category'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_rounded), label: 'profile'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline), label: 'app_info'),
+        ],
+        onTap: (index) {
+          setState(() {});
+          currentIndex = index;
+        },
+      ),
+    );
   }
 }
